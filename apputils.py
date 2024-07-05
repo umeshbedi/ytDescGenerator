9f37facc84aa22a4b0bee8ff0d2f9f848eaf3794
umeshbedi <ashramumesh@gmail.com>
2024-07-05 16:46:38 +0530

    sample app

---
+++ apputils.py
@@ -0,0 +1,7 @@
+import os
+
+from kivy.lang import Builder
+
+
+def load_kv(module_name):
+    Builder.load_file(f"{os.path.join(*module_name.split('.'))}.kv")
--- main.py
+++ main.py
@@ -1,1 +1,63 @@
-print("Now it is changed")
+# from kivy.uix.screenmanager import ScreenManager
+# # from kivymd.tools.hotreload.app import MDApp
+# from kivymd.app import MDApp
+
+
+# class SM(ScreenManager):
+#     def get_classes(self):
+#         return {screen.__class__.__name__: screen.__class__.__module__ for screen in self.screens}
+
+
+# class MainApp(MDApp):
+#     DEBUG = True
+#     sm = None
+#     state = {}
+
+#     def build_app(self, first=False):
+#         if self.sm is None:
+#             self.state = {'current': 'one',
+#                           'one': 'data one',
+#                           'two': 'data two'}
+#         else:
+#             self.state = {'current': self.sm.current,
+#                           'one': self.sm.get_screen('one').ids.data.text,
+#                           'two': self.sm.get_screen('two').ids.data.text}
+
+#         KV_FILES = []
+#         self.sm = SM()
+#         CLASSES = self.sm.get_classes()
+
+#         return self.sm
+
+#     def apply_state(self, state):
+#         self.sm.current = state['current']
+#         self.sm.get_screen('one').ids.data.text = state['one']
+#         self.sm.get_screen('two').ids.data.text = state['two']
+
+
+# if __name__ == '__main__':
+#     app = MainApp()
+#     app.run()
+
+#without hot reload
+
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+
+
+class SM(ScreenManager):
+    pass
+
+
+class MainApp(MDApp):
+    sm = None
+
+    def build(self):
+        self.sm = SM()
+        self.sm.current = 'one'
+        return self.sm
+
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
